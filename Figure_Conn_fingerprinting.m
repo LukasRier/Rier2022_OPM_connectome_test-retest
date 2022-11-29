5aacfcd35314dcdca0337f835c72dd440c79edcc
LukasRier <45269525+LukasRier@users.noreply.github.com>
2022-11-29 17:18:40 +0000

    Refactor, svg output and  better axis limits

--- Figure_Conn_fingerprinting.m
+++ Figure_Conn_fingerprinting.m
@@ -11,6 +11,7 @@ datadir = [project_dir,'data',filesep];
 
 hpfs = [4, 8,13,30,35,40];
 lpfs = [8,12,30,40,45,48];
+band_name = {'\theta','\alpha','\beta','\gamma_1','\gamma_2','\gamma_3'};
 
 if length(hpfs) ~= length(lpfs);error("Freq bands don't match");end
 if ~all((hpfs-lpfs)<0);error("Not all hipass smaller than lowpass");end
@@ -24,7 +25,7 @@ resf = fopen([results_dir,'FingerPrinting_results.csv'],'w')
 fprintf(resf,'Measure,mean,std.dev.\n');
 for f_ind = 1:length(hpfs)
 close all
-clearvars -except resf project_dir results_dir datadir hpfs lpfs f_ind frq_cols pval all_real_diffs all_mean_within_corrs
+clearvars -except resf min_corr max_corr  project_dir results_dir datadir hpfs lpfs f_ind frq_cols pval all_real_diffs all_mean_within_corrs band_name
 
 
 hp = hpfs(f_ind);
@@ -62,15 +63,15 @@ count = 0;
 for n = 1:10
     for m = 1:10
         count = count + 1;
-        run1sa = triu(AEC_b_all(:,:,n),1);
-        run2sb = triu(AEC_b_all2(:,:,m),1);
-        run1sa_v = run1sa(:);
-        run2sb_v = run2sb(:);
-        run1sa_v_t = run1sa_v(run1sa_v~=0);
-        run2sb_v_t = run2sb_v(run2sb_v~=0);
+        run1sa = AEC_b_all(:,:,n);
+        run2sb = AEC_b_all2(:,:,m);
+        run1sa_v_t = run1sa(mask);
+        run2sb_v_t = run2sb(mask);
         sub_correl(n,m) = corr(run1sa_v_t,run2sb_v_t);
     end
 end
+min_corr = -0.15 ;if min(sub_correl(:))<-0.15;error('change lims');end
+max_corr = 1; %max(sub_correl(:))
 %% Fingerprinting matrix
 figure(32767);
 pcolor(1:10,1:10,sub_correl);shading flat
@@ -162,7 +163,7 @@ plot(10*ones(size(S10_bet))+0.3*(rand(size(S10_bet))-0.5),S10_bet,'k.','linewidt
 hold on
 plot(10,S10_val,'r+','linewidth',4)
 
-axis([0 11 0 1])
+axis([0 11 min_corr max_corr])
 grid on
 ylabel('Correlation')
 xticks([1 2 3 4 5 6 7 8 9 10]);
@@ -174,7 +175,7 @@ set(gca,'fontsize',14)
 saveas(gcf,sprintf('%sFingerprinting_corr_scatter_%d_%d_Hz.png',results_dir,hp,lp));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure(104)
+figure(104);clf
 set(gcf,'position',[10,10,500,400])
 allwithin = diag(sub_correl);
 all_mean_within_corrs(f_ind) = mean(allwithin);
@@ -189,8 +190,11 @@ xticklabels({'Within subject', 'Between subject'});
 set(gca,'fontsize',14)
 set(gcf,'color',[1 1 1])
 xtickangle(0)
-axis([0.5 2.5 0 1])
+axis([0.5 2.5 min_corr max_corr])
+tx = text(gca,1.5,0.9,sprintf('%s',band_name{f_ind}),'FontSize',25,...
+    'HorizontalAlignment','center','VerticalAlignment','middle');
 saveas(gcf,sprintf('%sFingerprinting_within_between_bar_chart_%d_%d_Hz.png',results_dir,hp,lp));
+saveas(gcf,sprintf('%sFingerprinting_within_between_bar_chart_%d_%d_Hz.svg',results_dir,hp,lp));
 fprintf(resf,'Within-sub corr.(%d-%dHz),%1.8f,%1.8f\n',hp,lp,mean(allwithin),std(allwithin));
 fprintf(resf,'Between-sub corr.(%d-%dHz),%1.8f,%1.8f\n',hp,lp,mean(all_between),std(all_between));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -225,22 +229,21 @@ end
 fclose(resf);
 
 %% Identifiability plot
+
 f1 = figure();
-f1.Position([3,4]) = [1100,550];
-ax1 = subplot(2,1,1);
+f1.Position = [500,500,1100,300];
+ax1 = axes;
 hold on
-ax2 = subplot(2,1,2);
-hold on
+
 xlabel('Frequency (Hz)')
 set(f1,'Color','w')
-set([ax1,ax2],'FontSize',13,'TickLength',[0,0],'XTickLabelRotation',70,'XTick',unique([hpfs,lpfs]));
+set(ax1,'FontSize',13,'TickLength',[0,0],'XTickLabelRotation',70,'XTick',unique([hpfs,lpfs]));
 [~,sortid]= sort(pval);
 crit_pval_benjamini = 0.05./(20:-1:1);
 mincolor = 0.3;
 colorstep = 0.015;
 for f_ind = 1:length(hpfs)
-
-
+    
 center = (lpfs(f_ind) + hpfs(f_ind))./2;
 width = (lpfs(f_ind) - hpfs(f_ind))*0.975;
 val = all_real_diffs(f_ind);
@@ -250,12 +253,6 @@ if pval(f_ind) < crit_pval_benjamini(sortid(f_ind))
     th(f_ind) = text(ax1, center, val + 0.06, '*','FontSize',25,'HorizontalAlignment','center','VerticalAlignment','top');
     bh1(f_ind).FaceAlpha = 1;
 end
-val = all_mean_within_corrs(f_ind);
-
-bh2(f_ind) = bar(ax2,center,val,width,'FaceColor',frq_cols(f_ind,:),'FaceAlpha',0.8);
-if pval(f_ind) < crit_pval_benjamini(sortid(f_ind))
-    bh2(f_ind).FaceAlpha = 1;
-end
 end
 % bh1(5).Visible = 'off';
 % bh2(5).Visible = 'off';
@@ -263,9 +260,7 @@ end
 ax1.Position(1) = 0.1;ax2.Position(1) = 0.1;
 ax1.Position(3) = 0.85;ax2.Position(3) = 0.85;
 ax1.YLabel.String = sprintf('Identifiability');
-ax2.YLabel.String = sprintf('Correlation\n(Within-subject)');
 ax1.YLabel.FontSize = 13;
-ax2.YLabel.FontSize = 13;
-
+saveas(gcf,sprintf('%sFingerprinting_across_freqs.svg',results_dir));
 saveas(gcf,sprintf('%sFingerprinting_across_freqs.png',results_dir));
 save(sprintf('%sFingerprinting_across_freqs.mat',results_dir),'hpfs','lpfs','pval','all_real_diffs','all_mean_within_corrs');
