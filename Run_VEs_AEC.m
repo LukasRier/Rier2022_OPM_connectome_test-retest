3510898f6e8f6cb766dc0930852956cbb8a0b984
LukasRier <45269525+LukasRier@users.noreply.github.com>
2022-12-21 11:48:55 +0000

    clean up

--- Run_VEs_AEC.m
+++ Run_VEs_AEC.m
@@ -2,29 +2,23 @@ clear all
 close all
 clc
 
+% set project dir to directory containing data from https://doi.org/10.5072/zenodo.1134455
 
-% project_dir = pwd;project_dir(end-6:end)=[];
-project_dir =  '/net/cador/data_local/Lukas/movie/'
-% for sub_i =10
-%     sub = sprintf('%3d',sub_i);sub(sub == ' ') = '0'
-%     for ses_i = 1:2
-%         ses = sprintf('%3d',ses_i);ses(ses == ' ') = '0'
-%         resting_VE_noise_proj_func(sub,ses,project_dir)
-%     end
-% end
-%%
-for sub_i = 5:6
+project_dir =  'path/to/project/'
+
+%% Generate virtual electrodes and source power for MEG and empty room noise data
+for sub_i =1:10
+    sub = sprintf('%3d',sub_i);sub(sub == ' ') = '0'
+    for ses_i = 1:2
+        ses = sprintf('%3d',ses_i);ses(ses == ' ') = '0'
+        resting_VE_noise_proj_func(sub,ses,project_dir)
+    end
+end
+%% Estimate functional connectivity
+for sub_i = 1:10
     sub = sprintf('%3d',sub_i);sub(sub == ' ') = '0'
     for ses_i = 1:2
         ses = sprintf('%3d',ses_i);ses(ses == ' ') = '0'
         resting_conn_func(sub,ses,project_dir)
     end
 end
-
-% for sub_i =1:10
-%     sub = sprintf('%3d',sub_i);sub(sub == ' ') = '0'
-%     for ses_i = 1:2
-%         ses = sprintf('%3d',ses_i);ses(ses == ' ') = '0'
-%         resting_VE_pow(sub,ses,project_dir)
-%     end
-% end
